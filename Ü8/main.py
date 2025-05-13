3a35b16bf6f0c62531896a4df62d0e7c46a9ae53
ali fatali <aljoscha.becker1@gmail.com>
2025-05-14 00:20:14 +0200

    suizit

--- Ü8/main.py
+++ Ü8/main.py
@@ -1,16 +1,15 @@
-import numpy as np
 import time as t
 
 clock_delay = 0
 clk = 0
 
 class DL:
-    def __init__(self,D,E):
+    def __init__(self, D, E):
         self.D = D
         self.E = E
         Q = 0
 
-    def D_Latch(self,D, E,Q):
+    def D_Latch(D, E, Q):
         if E == 0:
             return Q
         elif E == 1:
@@ -24,26 +23,26 @@ class DFF():
         self.D = D
         self.clk = clk
         Q = 0
-         
-    def flipflop(self,D ,clk, Q):
+
+    def flipflop(D ,clk, Q):
         if clk == 1 & clock_delay == 0:
             Q = D
             return D
         else:
             return Q
 
-class wire:
-    def wire(self,state):
-        if state == 0:
-            return True
-        elif state == 1:
-            return False
-        else:
-            return ValueError
+class WIRE:
+    def __init__(self,state):
+        self.state = state
+    
+    def wire(state):
+        return state
+
+class REG:
+    def __init__(self,val):
+        self.val = val
+    def reg(self,val):
+        return val
     
 
-      
-def main():
-    B1 = DL(1, 1)
-    return
 
