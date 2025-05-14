1294fd1c620d8913ed29b929e983e6f79e5dce2a
ali fatali <aljoscha.becker1@gmail.com>
2025-05-15 00:48:39 +0200

    asdasdasdad

--- U8/volladierer.py
+++ U8/volladierer.py
@@ -8,22 +8,22 @@ start = t.time()
 clock_delay = 0
 clk = 0
 
-s = main.WIRE()
-a1 = main.AND()
-o1 = main.OR()
-xo1 = main.XOR()
+s = main.WIRE()                        # die gates in die Liste gesteckt zum benutzen 8-134 (aufgabenstellung ???)
+a1 = main.AND()                        # Problem liste lässt mich nichtmehr auf das objektr zugreifen ?????
+o1 = main.OR()                         # logic gates maybe als funktion und nicht als klasse ?????
+xo1 = main.XOR()                       
 n1 = main.NOT()
 s.add_and(a1)
 s.add_or(o1)
 s.add_xor(xo1)
 s.add_not(n1)
 
-def volladdierer(x,y ,cin):
-    b = s[0]
-    q = b.und2(x,y)
-    reg1 = main.REG(q)
-    b = s[2]
-    q = b.xor2[x,y]
+def volladdierer(x,y ,cin):             #einfach den schaltvorgang vom Addierer verfolgt 
+    b = s[0]                            #hab 2 und 2 xor und 1 oder gebraucht
+    q = b.und2(x,y)                     #reg1 = 1stes und reg2 erstes xor von x,y 
+    reg1 = main.REG(q)                  # reg 2 mit cin ge-and-ed und mit reg1 ge-oder't 
+    b = s[2]                            #denn für q das reg2 mit dem cin ge xor-ed 
+    q = b.xor2[x,y]                     #Problem von python unklar
     reg2 = main.REG(q)
     b = s[0]
     q = b.und2(cin,reg2)
