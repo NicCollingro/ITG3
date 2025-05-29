d798878a04b2a5f26c21ae6378cc6c3ebde73511
ali fatali <aljoscha.becker1@gmail.com>
2025-05-29 21:42:53 +0200

    HÖLLE

--- Verilog_Ali/12-170.v
+++ Verilog_Ali/12-170.v
@@ -1,7 +1,7 @@
 // TOPLEVEL oberstes Modul (oder ist mit toplevel sowas wie das int main(); in C gemeint ?) 
 module schaltung (clk, s,t,u);
     input clk;
-    output reg s = 1,t = 0,u = 0;
+    output reg s = 0,t = 0,u = 0;
     reg y;
     always @(posedge clk) begin
     s <= (s ^ t ^ u);
@@ -14,16 +14,32 @@ endmodule
 // und beide gates von dem flipßflop anbhängig sind
 
 module test1;
-    wire a, b, c;
+    reg a = 0, b = 0, c = 0;
     reg clk = 0;
-    always #5 clk = ~clk;
+    always #1 clk = ~clk;
     schaltung sch(.clk(clk), .s(a), .t(b), .u(c));
     always 
         #1 $display("%4t %b    | %b | %b | %b", $time , clk , a,b,c);
 
     initial begin 
+        $dumpfile("test");
+        $dumpvars(0,clk , a, b, c);
+
         $display("   T  clk | a | b | c |\n--------------------------------");
-        # 50 $finish;
+        # 2 a = 1;
+        # 4 a = 0;
+        # 6 b = 1;
+        # 8 b = 0;
+        # 10 c = 1;
+        # 12 c = 0;
+        # 14 a = 1; b = 1;
+        # 16 a = 0; b = 0;
+        # 18 a = 1; c = 1:
+        # 20 a = 0; c = 0;
+        # 22 b = 1; c = 1;
+        # 24 b = 0; c = 0;
+        # 26 a = 1; b = 1; c = 1; 
+        # 30 $finish;
     end
 endmodule;
 
