17f730fa47ceb3e60ff90e5339c75352a47a3498
NicCollingro <nic@collingro.de>
2025-06-02 18:49:57 +0200

    finished 12_173 Part 1,2

--- Uebungspack 12/12_173/mainPart1_2.v
+++ Uebungspack 12/12_173/mainPart1_2.v
@@ -7,7 +7,8 @@ module top_level();
     always @(posedge clk) cnt = cnt + 1;
 
     reg red = 1, yellow = 1, green = 0;
-    reg red2 = 0, yellow2 = 0, green2 = 0;
+    wire red2 = 0, yellow2 = 0, green2 = 0;
+    reg button = 0;
 
     always #1 $display("%b %f %b %b %b", clk, cnt, red, yellow, green);
 
@@ -22,12 +23,15 @@ module top_level();
 
     //Strukturbeschreibung
 
-    //Die Strukturbeschreibung ist in der Musterlösung genau gleich der Verhaltensbeschreibung
+    // Mit assign auf wires anstatt in extra registern zu speichern und ohne always, da einmal zugewiesen immer den Wert dessen ausgibt, was er zugewiesen bekommen hat.
 
+    assign red2 = (~cnt[0] & ~cnt[1] & ~cnt[2]) | (cnt[2] & (cnt[1] | cnt[0]));
+    assign yellow2 = (~cnt[2] & cnt[0] & cnt[1]) | (~cnt[0] & ~cnt[1] & ~cnt[2]) | (cnt[2] & ~cnt[0] & ~cnt[1]);
+    assign green2 = ~cnt[2] & ( cnt[1] & ~cnt[0] | ~cnt[1] & cnt[0]);
 
     initial begin
         $dumpfile("testbench.vcd"); 
-        $dumpvars(0, clk, cnt, red, yellow, green);
+        $dumpvars(0, clk, cnt, red, yellow, green, red2, yellow2, green2);
         #40 $finish; 
     end
 
