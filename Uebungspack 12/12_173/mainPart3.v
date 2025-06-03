a60b0dd507a1746601b8b756f3193391fa469c4e
NicCollingro <nic@collingro.de>
2025-06-03 10:30:53 +0200

    update

--- Uebungspack 12/12_173/mainPart3.v
+++ Uebungspack 12/12_173/mainPart3.v
@@ -6,33 +6,31 @@ module top_level();
     reg[2:0] cnt = 0;       //erstelle 3 bit großen bus als counter
     always @(posedge clk) cnt = cnt + 1;
 
-    reg red = 1, yellow = 1, green = 0;
-    wire red2 = 0, yellow2 = 0, green2 = 0;
+    wire red = 1, yellow = 1, green = 0;
+
     reg button = 0;
+
+    always @(cnt == 0) button = ~button;
 
     always #1 $display("%b %f %b %b %b", clk, cnt, red, yellow, green);
 
-    
-    //Verhaltensbeschreibung
-
-    always @(cnt) begin // immer wenn sich count ändert aufrufen
-        red <= (~cnt[0] & ~cnt[1] & ~cnt[2]) | (cnt[2] & (cnt[1] | cnt[0]));
-        yellow <= (~cnt[2] & cnt[0] & cnt[1]) | (~cnt[0] & ~cnt[1] & ~cnt[2]) | (cnt[2] & ~cnt[0] & ~cnt[1]);
-        green <= ~cnt[2] & ( cnt[1] & ~cnt[0] | ~cnt[1] & cnt[0]);
+    always @(cnt == 0 & ~button) begin
+         // immer wenn sich count ändert aufrufen
+        assign red <= (~cnt[0] & ~cnt[1] & ~cnt[2]) | (cnt[2] & (cnt[1] | cnt[0]));
+        assign yellow <= (~cnt[2] & cnt[0] & cnt[1]) | (~cnt[0] & ~cnt[1] & ~cnt[2]) | (cnt[2] & ~cnt[0] & ~cnt[1]);
+        assign green <= ~cnt[2] & ( cnt[1] & ~cnt[0] | ~cnt[1] & cnt[0]);
     end
 
-    //Strukturbeschreibung
-
-    // Mit assign auf wires anstatt in extra registern zu speichern und ohne always, da einmal zugewiesen immer den Wert dessen ausgibt, was er zugewiesen bekommen hat.
-
-    assign red2 = (~cnt[0] & ~cnt[1] & ~cnt[2]) | (cnt[2] & (cnt[1] | cnt[0]));
-    assign yellow2 = (~cnt[2] & cnt[0] & cnt[1]) | (~cnt[0] & ~cnt[1] & ~cnt[2]) | (cnt[2] & ~cnt[0] & ~cnt[1]);
-    assign green2 = ~cnt[2] & ( cnt[1] & ~cnt[0] | ~cnt[1] & cnt[0]);
+    always @(cnt == 0 & button) begin
+        assign red = 1;
+        assign yellow = 0;
+        assign green = 0;
+    end
 
     initial begin
         $dumpfile("testbench.vcd"); 
         $dumpvars(0, clk, cnt, red, yellow, green, red2, yellow2, green2);
-        #40 $finish; 
+        #40 $finish;
     end
 
 endmodule
