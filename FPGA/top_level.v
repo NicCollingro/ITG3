37d3584d21df3e5b16134f3aa841f3f0136cf774
reali madrid <aljoscha.becker1@gmail.com>
2025-06-10 10:55:17 +0200

    yahooo

--- FPGA/top_level.v
+++ FPGA/top_level.v
@@ -1,33 +1,13 @@
-module top_level(CLOCK_50, key0, led);
+module top_level(CLOCK_50,GPIO_023,GPIO_021,
+ GPIO_019,GPIO_017, GPIO_015,GPIO_013,GPIO_011);
+input CLOCK_50, ;
 
-  input CLOCK_50, key0;
-  output[7:0] led;
+always
 
-  reg[26:0] counter;
-  reg[5:0]  PWM_adj;
-  reg[6:0]  PWM_width;
-  reg[7:0]  led;
+endmodule
 
-  always @(posedge CLOCK_50 or negedge key0) begin
-    if (!key0) begin
-      counter <= 0;
-      led[0]  <= 0;
-    end else begin
-      counter   <= counter+1;
-      PWM_width <= PWM_width[5:0]+ PWM_adj;
-      if (counter[26]) begin
-        PWM_adj <= counter[25:20];
-      end else begin
-        PWM_adj <= ~counter[25:20];
-      end
-      led[0] <= ~PWM_width[6];
-      led[1] <= ~PWM_width[6];
-      led[2] <= ~PWM_width[6];
-      led[3] <= ~PWM_width[6];
-      led[4] <=  PWM_width[6];
-      led[5] <=  PWM_width[6];
-      led[6] <=  PWM_width[6];
-      led[7] <=  PWM_width[6];
-    end
-  end
+module keypad(input clk; output reg [3:0] keycode,
+input [2:0] cols, output reg [3:0] rows);
+
+
 endmodule
