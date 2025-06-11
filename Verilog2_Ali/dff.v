1efb2e64c3177442ab4698e45d1dd70a91127b31
reali madrid <aljoscha.becker1@gmail.com>
2025-06-11 13:13:45 +0200

    pewjfwjfwejf

--- Verilog2_Ali/dff.v
+++ Verilog2_Ali/dff.v
@@ -1,9 +1,32 @@
-module D_FlipFlop(input wire D, input wire clk, output Q);
+module top_level();
 
-assign clk = !clk;
+reg D,clk;
+
+always #1 clk = ~clk;
+wire Q;
+
+D_FlipFlop flopper (.D(D), .clk(clk), .Q(Q));
+
+initial begin
+    D  = 0; clk = 0;
+    $dumpfile("test");
+    $dumpvars(0,clk , D, Q);
+
+
+    #2 D = 1;clk = 0;
+    #4 D = 0;
+    #2 D = 1;
+    #2 D = 0;
+    #4 D = 1;
+end
+
+endmodule
+
+
+
+module D_FlipFlop(input wire D, input wire clk, output wire Q);
 wire mitte;
-D_Latch Latch1(.D(D), .clk(clk), .Q(mitte));
-assign clk = !clk;
+D_Latch Latch1(.D(D), .clk(~clk), .Q(mitte));
 D_Latch Latch2(.D(mitte), .clk(clk), .Q(Q));
 
 endmodule
@@ -11,13 +34,12 @@ endmodule
 module D_Latch(input wire D, input wire clk, output Q);
 wire a;
 wire b;
-
-assign a = !((!D) && clk);
-assign b = !(D && clk);
-
 wire nq;
 
-assign Q = !(a && nq);
-assign nq = !(b && Q); 
+assign a = ~(~D & clk);
+assign b = ~(D & clk);
+
+assign Q = ~(a & nq);
+assign nq = ~(b & Q); 
 
 endmodule
