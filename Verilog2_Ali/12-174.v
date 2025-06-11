b98c4c60f94581556c36aabf644788628c72060a
reali madrid <aljoscha.becker1@gmail.com>
2025-06-11 14:02:51 +0200

    idfdnojdsnvodsnvdsnvdspnvdsnvdsnvvnvkbvvökjsbvfs

--- Verilog2_Ali/12-174.v
+++ Verilog2_Ali/12-174.v
@@ -1,3 +1,24 @@
-module top_level();
+`include "dff.v"
 
+module top_level();
+reg D,clk;
+always #1 clk = ~clk;
+wire Q;
+
+D_FlipFlop flopper (.D(D), .clk(clk), .Q(Q));
+
+initial begin
+    D  = 0;
+    clk = 0;
+    $dumpfile("test");
+    $dumpvars(0,D,clk,Q);
+
+
+    # 2 D = 1;
+    # 4 D = 0;
+    # 2 D = 1;
+    # 2 D = 0;
+    # 4 D = 1;
+    # 10 $finish;
+end
 endmodule
