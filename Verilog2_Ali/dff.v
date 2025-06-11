a98a01354329dd00151845596081624b3a7ca275
reali madrid <aljoscha.becker1@gmail.com>
2025-06-11 14:02:51 +0200

    idfdnojdsnvodsnvdsnvdspnvdsnvdsnvvnvkbvvökjsbvfs

--- Verilog2_Ali/dff.v
+++ Verilog2_Ali/dff.v
@@ -1,45 +1,10 @@
-module top_level();
-
-reg D,clk;
-
-always #1 clk = ~clk;
-wire Q;
-
-D_FlipFlop flopper (.D(D), .clk(clk), .Q(Q));
-
-initial begin
-    D  = 0; clk = 0;
-    $dumpfile("test");
-    $dumpvars(0,clk , D, Q);
-
-
-    #2 D = 1;clk = 0;
-    #4 D = 0;
-    #2 D = 1;
-    #2 D = 0;
-    #4 D = 1;
-end
-
-endmodule
-
-
+`include "dlat.v"
 
 module D_FlipFlop(input wire D, input wire clk, output wire Q);
 wire mitte;
-D_Latch Latch1(.D(D), .clk(~clk), .Q(mitte));
-D_Latch Latch2(.D(mitte), .clk(clk), .Q(Q));
+
+D_Latch Latch (.D(D), .clk(~clk), .Q(mitte));
+D_Latch catch (.D(mitte), .clk(clk), .Q(Q));
 
 endmodule
 
-module D_Latch(input wire D, input wire clk, output Q);
-wire a;
-wire b;
-wire nq;
-
-assign a = ~(~D & clk);
-assign b = ~(D & clk);
-
-assign Q = ~(a & nq);
-assign nq = ~(b & Q); 
-
-endmodule
