27551fbad1521c7c727278a6c561fcbdb0fca5a4
ali fatali <aljoscha.becker1@gmail.com>
2025-06-11 18:43:35 +0200

    adsoifhj

--- Verilog2_Ali/12-174.v
+++ Verilog2_Ali/12-174.v
@@ -13,7 +13,6 @@ initial begin
     $dumpfile("test");
     $dumpvars(0,D,clk,Q);
 
-
     # 2 D = 1;
     # 4 D = 0;
     # 2 D = 1;
@@ -21,4 +20,27 @@ initial begin
     # 4 D = 1;
     # 10 $finish;
 end
+endmodule
+module FeedbackOszil (input wire clk, output wire x, input wire reset, output wire y, );
+    reg rest;
+    reg D1,D2;
+    
+    always @(posedge clk) begin         //Habs mal mit nem Multiplexer versucht;
+        case (reset)                    //Der is ja Kombinatorisch
+            1'b1: begin                 // wusste aber nicht wie ich
+                D1 <= 0;                // Kombinatorisch die Wire zuweise
+                D2 <= 1;
+            end 
+            default: begin
+                D1 <= y;
+                D2 <= x;
+            end
+        endcase
+
+    end
+
+    D_FlipFlop flotti  (.D(D1), .clk(clk), .Q(x));
+    D_FlipFlop karotti (.D(D2), .clk(clk), .Q(y));
+
+    
 endmodule
