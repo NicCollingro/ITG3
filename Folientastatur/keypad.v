42c0175d3e97214e891ea04b8e1a1e2649f3f39f
NicCollingro <nic@collingro.de>
2025-06-13 17:39:11 +0200

    .

--- Folientastatur/keypad.v
+++ Folientastatur/keypad.v
@@ -1,10 +1,10 @@
-module keycode(input CLOCK_50, input [2:0] cols, output reg [3:0] rows, output reg [3:0] keycode);
+module keycode(input CLOCK_50, input [3:0] cols, output reg [3:0] rows, output reg [3:0] keycode);
 
     reg [19:0] counter; 
 
     wire clk;
 
-    reg [2:0] rows_slc = 0;
+    reg [1:0] rows_slc = 0;
 
     always @(posedge CLOCK_50) begin
         counter = counter + 1;
@@ -17,18 +17,24 @@ module keycode(input CLOCK_50, input [2:0] cols, output reg [3:0] rows, output r
         rows <= ~(4'b0001 << rows_slc);
     end
 
-    case({rows, cols})
-        7'b1110_110: keycode <= 4'd0;
-        7'b1110_101: keycode <= 4'd1;
-        7'b1110_011: keycode <= 4'd2;
-        7'b1101_110: keycode <= 4'd3;
-        7'b1101_101: keycode <= 4'd4;
-        7'b1101_011: keycode <= 4'd5;
-        7'b1011_110: keycode <= 4'd6;
-        7'b1011_101: keycode <= 4'd7;
-        7'b1011_011: keycode <= 4'd8;
-        7'b0111_110: keycode <= 4'd9;
-        7'b0111_101: keycode <= 4'd10;
-        7'b0111_011: keycode <= 4'd11;
-    endcase
+    always @(posedge clk) begin
+        case({rows, cols})
+            8'b1110_1110: keycode <= 4'd0;
+            8'b1110_1101: keycode <= 4'd1;
+            8'b1110_1011: keycode <= 4'd2;
+            8'b1110_0111: keycode <= 4'd3;
+            8'b1101_1110: keycode <= 4'd4;
+            8'b1101_1101: keycode <= 4'd5;
+            8'b1101_1011: keycode <= 4'd6;
+            8'b1101_0111: keycode <= 4'd7;
+            8'b1011_1110: keycode <= 4'd8;
+            8'b1011_1101: keycode <= 4'd9;
+            8'b1011_1011: keycode <= 4'd10;
+            8'b1011_0111: keycode <= 4'd11;
+            8'b0111_1110: keycode <= 4'd12;
+            8'b0111_1101: keycode <= 4'd13;
+            8'b0111_1011: keycode <= 4'd14;
+            8'b0111_0111: keycode <= 4'd15;
+        endcase
+    end
 endmodule
---
+++ Folientastatur/top_level.v
@@ -0,0 +1,9 @@
+`include "keypad.v"
+
+module top_level(input wire CLOCK_50, input wire key0, key1, input wire GPIO_023, GPIO_021, GPIO_019, GPIO_017, output reg GPIO_015,GPIO_013,GPIO_011,GPIO_009, output wire [7:0] led);
+    wire [7:0] keycode;
+
+    keypad KBD(.clk(CLOCK_50), .cols({GPIO_023, GPIO_021, GPIO_019, GPIO_017}), .rows({GPIO_015,GPIO_013,GPIO_011,GPIO_009}), .keycode(keycode));
+
+    assign led = keycode;
+endmodule
