886d9821d3e9f1e5b6f2ad28187a0d9f0b2e3290
ali fatali <aljoscha.becker1@gmail.com>
2025-06-19 01:17:15 +0200

    dofhsdjfhbnsdikjfhsdjfhskjldfhskdjöfhsdökljfhsdöolgfjsdhöoflhöogikjhsöogihsöoig

--- Verilog3_Ali/12-179.v
+++ Verilog3_Ali/12-179.v
@@ -1,42 +1,54 @@
 module top_level ();
-reg clk = 0;
-always #1 clk = ~clk;
+    reg clk = 0;
+    always #1 clk = ~clk;
+    reg sda_r = 0;
+    reg oe = 0;
+    wire sda;
+    assign sda = oe ? 1'bz : sda_r; // rumgedreht hier weil sonst huddel mit volcro
+    reg [7:0] wr = 8'b10011101;
+    reg dout = 0;
 
-reg sda_r;
-reg oe = 0;
-wire inout sda;
+    volcol fokke (.clk(clk), .sda(sda), .oe(oe));
+
+    always @(posedge clk) begin
+        dout <= wr[7];
+        if (oe == 1'b1) begin 
+
+        end else begin
+            sda_r <= wr[7]; 
+            wr <= wr << 1;   
+        end
+    end
+
+    initial begin
+        $dumpfile("test.vcd");
+        $dumpvars(0, clk, sda, oe, dout);  
+        oe = 0;
+        #17 
+        #2 oe = 1'bz;// Hier weil sonst beim oe wechsel ein Takt übersprungen
+        oe = 1;      // wo das am code liegt ist mir unklar mein guess sind die non-blocking
+        #16$finish;  // Finde aber den Fehler nicht also keine Ahnung   
+    end              // NEUER GUESS wechsel sind ja nicht instantan also im Default oder 1'd1 case
+endmodule           
+
+
+module volcol (input wire clk, inout wire sda, input wire oe);
+reg sda_r = 0;
 assign sda = oe ? sda_r : 1'bz;
-reg io1;
-reg [7:0] wr = 8'b10100101;
+reg [7:0] Din = 8'd0;
 
-volcol fokke (.clk(clk), .sda(sda), .oe(oe));
-
-always @(posedge clk) begin
-    case
-
-
-
-        default: sda_r <= x;
-    endcase
-end
-endmodule
-
-module volcol (input wire clk, inout sda, input wire ~oe)
-reg sda_r;
-assign sda = oe ? sda_r : 1'bz;
-reg [7:0] Din;
 always @(posedge clk) begin
     case (oe)
-        1'd0: begin
-            Din[0] <= sda_r;
-            Din[0] <= Din << 1'b1; 
-        end
         1'd1: begin
-            Din[0] <= sda_r;
-            Din[0] <= Dinn >> 1'd1;
+            sda_r <= Din[0];
+            Din <= Din >> 1'b1;
+        end
+        1'd0: begin
+            Din <= {Din[6:0], sda}; 
         end
 
-        default: sda_r <= x; 
-endcase
-end
+        default: sda_r <= 1'bz;
+        endcase
+    end
+    
 endmodule
