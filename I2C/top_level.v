aa2761c9be836fea2c2018758315b1869df1017e
ali fatali <aljoscha.becker1@gmail.com>
2025-06-18 22:07:05 +0200

    saassad

--- I2C/top_level.v
+++ I2C/top_level.v
@@ -1,6 +1,22 @@
 `include "i2cmaster.v"
 
-module top_level(
+module top_level ();
+
+reg clk = 0; 
+always #1 clk <= ~clk;
+wire G32, G33;
+
+top_level1 topper(.CLOCK_50(clk), .GPIO_032(G32), .GPIO_033(G33));
+
+initial begin
+    $dumpfile("test.vcd");
+    $dumpvars(0,clk,G32,G33);
+    # 100 $finish;
+end
+    
+endmodule
+
+module top_level1(
     input wire CLOCK_50,
     inout wire GPIO_032,
     output wire GPIO_033
@@ -8,7 +24,7 @@ module top_level(
 
 reg rw = 0;
 reg send = 0;
-reg [6:0] addr = 7'b0111011;    
+reg [6:0] addr = 7'b0111011;
 reg [7:0] data = 8'b01111101;
 wire busy;
 
@@ -21,5 +37,5 @@ end
 
 I2C_master masti (.CLOCK_50(CLOCK_50), .sda(GPIO_032), .scl(GPIO_033),
     .send(send), .busy(busy), .addr(addr), .data(data), .rw(rw));
-
+    
 endmodule
