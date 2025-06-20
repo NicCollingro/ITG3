4e2457b0d6a9924e41da4d84ef759e5d0c0d010e
reali madrid <aljoscha.becker1@gmail.com>
2025-06-20 17:27:46 +0200

    1 fefwf

--- CoCC/regblock.v
+++ CoCC/regblock.v
@@ -1,7 +1,8 @@
 module regblock (input wire clk input wire we, 
 input wire[2:0] oaddr, input wire[2:0] iaddr,
 input wire[7:0] idata, input wire oe,
-output wire[7:0] odata); 
+output wire[7:0] odata, output wire [7:0] rega
+output wire [7:0] regb); 
 reg[7:0] registers[0:7];
 
 assign rega = registers[0];
