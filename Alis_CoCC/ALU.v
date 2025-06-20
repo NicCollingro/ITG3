bf27b1480302cede0047580fdc1becffb6981233
reali madrid <aljoscha.becker1@gmail.com>
2025-06-20 17:56:34 +0200

    COCCV2

--- Alis_CoCC/ALU.v
+++ Alis_CoCC/ALU.v
@@ -1,3 +1,16 @@
-module ALU();
+module ALU(input wire [7:0] in_a, imput wire [7:0] in_b, 
+input wire [2:0] mode, input wire eo,
+output wire [7:0] out, output reg flag_zero = 0, 
+output reg flag_carry = 0, input wire ee);
+reg [7:0] r_out = 8'b0;
+assign out = (eo) ?
+
+
+
+
+
+
+
+
 
 endmodule
--- CoCC/tristate_buffer.v
+++
@@ -1,3 +0,0 @@
-module tristate_buffer (input wire oe, inout wire [7:0] out, input wire [7:0] in);
-assign out = (oe) ? in : 8'bz;
-endmodule
