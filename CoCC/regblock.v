28657ad9fa78edea64e37f96c8c434ae8c0db5f5
reali madrid <aljoscha.becker1@gmail.com>
2025-06-20 17:26:17 +0200

    BESSERER COCC

--- CoCC/regblock.v
+++ CoCC/regblock.v
@@ -4,9 +4,12 @@ input wire[7:0] idata, input wire oe,
 output wire[7:0] odata); 
 reg[7:0] registers[0:7];
 
+assign rega = registers[0];
+assign regb = registers[1];
 
+assign odata = (oe) ? registers[oaddr] : 8'bz;
 always @(posedge clk) begin
-
-
+    if (we)
+        register[iaddr] <= idata;
 end
-endmodule
+endmodule 
