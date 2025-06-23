605baeee43cadbd9d91470246afb07c4566362ab
reali madrid <aljoscha.becker1@gmail.com>
2025-06-23 16:30:32 +0200

    sodkfsonf

--- Alis_CoCC/ram.v
+++ Alis_CoCC/ram.v
@@ -1,5 +1,17 @@
 module ram ( input wire clk, input wire we, input wire oe,
-input wire [7:0] addr    
+input wire [7:0] addr, inout wire [7:0] bus
 );
 reg[7:0] mem[0:255];
+reg [7:0] r;
+assign bus = (oe & we) ? r : 8'bz;
+
+always @(posedge clk) begin
+if (oe) begin
+    if (we) begin
+        r <= mem[addr];
+    end else begin
+        mem[addr] <= bus
+    end
+end
+end
 endmodule
