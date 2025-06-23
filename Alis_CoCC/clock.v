8edfba6e91f8ce691dc01aa2857878aa5db6dbae
reali madrid <aljoscha.becker1@gmail.com>
2025-06-23 17:35:15 +0200

    kann net Programmieren :(

--- Alis_CoCC/clock.v
+++ Alis_CoCC/clock.v
@@ -5,14 +5,15 @@ output reg cycle_clk = 0, output reg ram_clk = 0, output reg internal_clk = 0
 reg [2:0] cnt = 3'd1;
 
 always @(posedge clk) begin
-    if (cnt == 3'd4) begin
-        cnt <= 3'd1;
-    end else begin
-        cnt <= {cnt[1:0],0};
-    end
+    case (cnt)
+        3'd1: cnt <= {cnt[1:0],1'd0};
+        3'd2: cnt <= {cnt[1:0],1'd0};
+        3'd4: cnt <= 3'd1;
+        default: cnt = 3'dx; //für schlechten code :((
+    endcase
 
     cycle_clk <= cnt[0];
     ram_clk <= cnt[1];
-    internal_clk <= [2];
+    internal_clk <= cnt[2];
 end
 endmodule
