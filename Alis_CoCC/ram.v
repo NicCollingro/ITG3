2bf9393c6254ae9fb5e47dc0f54ddbb811614911
reali madrid <aljoscha.becker1@gmail.com>
2025-06-23 17:28:20 +0200

    habibus

--- Alis_CoCC/ram.v
+++ Alis_CoCC/ram.v
@@ -3,19 +3,15 @@ input wire [7:0] addr, inout wire [7:0] bus
 );
 reg[7:0] mem[0:255];
 reg [7:0] r;
-assign bus = (oe & we) ? r : 8'bz;
+assign bus = (oe & ~we) ? mem[addr] : 8'bz;
 
 always @(posedge clk) begin
-if (oe) begin
     if (we) begin
-        r <= mem[addr];
-    end else begin
-        mem[addr] <= bus
+        mem[addr] <= bus;
     end
-end
 end
 
 initial begin
-    $readmemh("rom.hex", mem)
+    $readmemh("rom.hex", mem);
 end
 endmodule
