0fa08a6a4d91b748eb12392d3f713e02ed2b7675
ali fatali <aljoscha.becker1@gmail.com>
2025-06-25 20:21:38 +0200

    LALALALAL

---
+++ Alis_CoCC/top_level.v
@@ -0,0 +1,17 @@
+module top_level (input wire CLOCK_50, output wire [7:0] wire led);
+
+reg [24:0] help;
+reg clk;
+always @(posedge CLOCK_50) begin
+    help <= help 1'd1;
+    clk <= help[23];
+end
+
+computer combo(.clk(clk));
+
+initial begin
+        $dumpfile("test.vcd");
+        $dumpvars(0, combo); 
+end
+
+endmodule
