fe44aba9c8cdbdab2ce3b8863ec5ce3cd49976ed
reali madrid <aljoscha.becker1@gmail.com>
2025-06-27 16:41:04 +0200

    BESSER2

--- Alis_CoCC/top_level.v
+++ Alis_CoCC/top_level.v
@@ -3,7 +3,7 @@ module top_level (input wire CLOCK_50, output wire [7:0] led);
 reg [24:0] help;
 reg clk;
 always @(posedge CLOCK_50) begin
-    help <= help 1'd1;
+    help <= help + 1'd1;
     clk <= help[23];
 end
 
