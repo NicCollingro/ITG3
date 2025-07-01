0029a6eb51b42b6a64a2f56a0a9855055d4ca758
archliÂ <aljoscha.becker1@gmail.com>
2025-07-01 11:59:17 +0200

    usa

---
+++ COCC-SIM/asm-code.asm
@@ -0,0 +1,11 @@
+.text 
+    ldx A %x
+    ldx B %y
+    ldi C 0
+    
+
+
+    hlt
+.data 
+    x=20
+    y=10
--- COCC-SIM/cocc-sim.sh
+++ COCC-SIM/cocc-sim.sh
@@ -4,6 +4,6 @@ prog="cocc-sim_col.py"
 
 #gnome-terminal --full-screen -- python3 asm-sim.py $1
 #gnome-terminal --geometry=135x40 --full-screen -- 
-python3 ${prog} $1
+python3 ${prog} asm-code.asm
 
 
