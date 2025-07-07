c1f1f92f85c3d141aa816ca30eb2caff3e5a8e3b
archliÂ <aljoscha.becker1@gmail.com>
2025-07-07 20:12:41 +0200

    vd

--- COCC-SIM/asm-code.asm
+++ COCC-SIM/asm-code.asm
@@ -1,9 +1,39 @@
 .text 
-    ldi A 0 
-    ldo B 0
-    ldi C 0
-    
+    lda E %x 
+    ldi F 0 ;Help
+    ldi G 0 ;End
+    ldi B 0
+    push B
+    push B
+start:
+    pop B 
+    lda A %x
+    cmp 
+    jz %end ;wenn 
 
+    pop B
+    ldi A 0 ;0 für Help 
+    cmp     ;1 für End
+    jz %ringHelp
+    jmp %ringEnd
+
+end:
 
     hlt
+
+ringHelp:
+    mov B F
+    lda A %x
+    cmp 
+    jz %helpEnd
+
+    
+
+ringEnd:
+
+helpEnd:
+    mov G H
+    jmp %end
+
 .data 
+x = 20
