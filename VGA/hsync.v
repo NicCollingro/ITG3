f98387b097f75e48be9f6314fc357bed3f861c86
ali fatali <aljoscha.becker1@gmail.com>
2025-07-13 14:13:20 +0200

    Albpentraum

--- VGA/hsync.v
+++ VGA/hsync.v
@@ -1,28 +1,12 @@
+`include "symbols.vh"
+
 module hsync(input wire i_clk, output wire o_hsync 1'd1, output reg o_hblank = 1'd0);
-    reg [4:0] out = 5'd0;
+    reg [11:0] cnt = 12'd0;
+
     always @(posedge i_clk) begin
-        case (out)
-            3'd0: begin
-                out <= out + 5'd1;
-                o_hblank <= 1'd0;
-                o_hsync <= 1'd1;
-            end 
-            3'd22: begin
-                out <= out + 5'd1;
-                o_hblank <= 1'd1;
-            end 
-            3'd26: begin
-                out <= out + 5'd1;
-                o_hblank <= 1'd1;
-                o_hsync <= 1'd0;
-            end 
-            3'd29: begin
-                out <= out + 5'd1;
-                o_hblank <= 1'd1;
-                o_hsync <= 1'd1;
-            end
-            default: out <= out + 5'd1;
-        endcase
+        cnt <= (cnt < `HTOT) ? count + 12'd1 : 12'd0;
+        o_hsync <= (`HSRT <= cnt && cnt <= `HEND) ? 0 : 1;
+        o_hblank <= (cnt > `HRES) ? 1 : 0;
     end
 
 endmodule
--- VGA/vga.v
+++ VGA/vga.v
@@ -1,20 +1,25 @@
 module vga (
-input wire CLOCK_25,
+input wire CLOCK_50,
 output wire o_hsync,
 output wire o_vsync,
 output wire o_red,
 output wire o_grn,
 output wire o_blu
 );
+reg clk= 0;
+always @(CLOCK_50) begin
+    clk = ~clk;
+end
 
-reg [11:0] x,y;
-
-
-// hsync und vsync hab ich ja mit der negedge von hsync in 
-// Vsync implementiert weil trust is geuil so habibi
-/*
-hsync hippi ( .i_clk(CLOCK_25), .o_hsync(o_sync), .o_hblank);
+reg [11:0] ix = 0,y = 0;
+wire o_hblank, o_vblank;
+hsync hippi ( .i_clk(clk), .o_hsync(o_hsync), .o_hblank(o_hblank));
 vsync vsynchi ( .i_clk(o_blank), .o_vsync(o_vsync), .o_vblank(o_vblank));
-*/
+
+always @(posedge clk) begin
+    ix <= o_hblank ? 0 : ix + 1;
+    y <= o_vblank ? ß : y + 1;
+end
+
 
 endmodule
