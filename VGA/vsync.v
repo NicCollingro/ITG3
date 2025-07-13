884e38d468c8259d0212abc5019a0729d5400dc4
ali fatali <aljoscha.becker1@gmail.com>
2025-07-13 14:13:20 +0200

    Albpentraum

--- VGA/vsync.v
+++ VGA/vsync.v
@@ -1,31 +1,12 @@
+`include "symbols.vh"
+
 module vsync(input wire i_clk, output reg o_vsync = 1'd1, output reg o_vblank = 1'd0);
-reg [3:0] out = 4'd0;
 
-always @(negedge i_clk) begin
-    case (out)
-        4'd0: begin
-            out <= out + 4'd1;
-            o_vsync <= 1'd1;
-            o_vblank <= 1'd0;
-        end
-        4'd9: begin
-            out <= out + 4'd1;
-            o_vsync <= 1'd1;
-            o_vblank <= 1'd1;
-        end
-        4'd11: begin
-            out <= out + 4'd1;
-            o_vsync <= 1'd0;
-            o_vblank <= 1'd1;
-        end
-        4'd12: begin
-            out <= out + 4'd1;
-            o_vsync <= 1'd1;
-            o_vblank <= 1'd1;
-        end
-        default: out <= out + 4'd1;
-    endcase
+reg [11:0] cnt = 12'd0;
 
-
-end
+    always @(posedge i_clk) begin
+        cnt <= (cnt < `VTOT) ? count + 12'd1 : 12'd0;
+        o_hsync <= (`VSRT <= cnt && cnt <= `VEND) ? 0 : 1;
+        o_hblank <= (cnt > `VRES) ? 1 : 0;
+    end
 endmodule
