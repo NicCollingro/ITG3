0310e3cf3f62a9d5de9d1593a52d704e2a40a12d
ali fatali <aljoscha.becker1@gmail.com>
2025-07-13 22:04:21 +0200

    idk man

--- VGA/vga.v
+++ VGA/vga.v
@@ -11,14 +11,15 @@ always @(CLOCK_50) begin
     clk = ~clk;
 end
 
-reg [11:0] ix = 0,y = 0;
+reg [11:0] ix [11:0],
+reg [11:0] y = 0;
 wire o_hblank, o_vblank;
 hsync hippi ( .i_clk(clk), .o_hsync(o_hsync), .o_hblank(o_hblank));
 vsync vsynchi ( .i_clk(o_blank), .o_vsync(o_vsync), .o_vblank(o_vblank));
 
 always @(posedge clk) begin
-    ix <= o_hblank ? 0 : ix + 1;
-    y <= o_vblank ? ß : y + 1;
+    ix[y] <= o_hblank ? 0 : ix + 1;
+    y <= o_vblank ? 0 : y + 1;
 end
 
 
