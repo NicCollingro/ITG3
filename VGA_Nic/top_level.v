8c1fbcb60c039c20c613be5acb57f12cb74f87ff
NicCollingro <nic@collingro.de>
2025-07-14 18:57:17 +0200

    top_level VGA

---
+++ VGA_Nic/top_level.v
@@ -0,0 +1,13 @@
+`include VGA.v
+module top_level(input wire CLOCK_50, output wire GPIO_000, output  wire GPIO_001, output wire GPIO_007, output wire GPIO_007, output wire GPIO_005, output wire GPIO_003);
+    wire R,G,B,HSYNC,VSYNC;
+
+    assign R = GPIO_007;
+    assign G = GPIO_005;
+    assign B = GPIO_003;
+
+    assign HSYNC = GPIO_000;
+    assign VSYNC = GPIO_001;
+
+    vga test(.CLOCK_50(CLOCK_50), .isel(0), .o_hsync(HSYNC), .o_vsync(VSYNC), .o_red(R), .o_grn(G), .o_blu(B))
+endmodule
