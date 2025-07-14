ee6d90cfe434f0aab5a1bbb9d7399d0408e9a96a
leidus <aljoscha.becker1@gmail.com>
2025-07-14 19:01:46 +0200

    lol

--- VGA/top_level.v
+++ VGA/top_level.v
@@ -1,3 +1,9 @@
-module top_level ();
+module top_level (
+input wire CLOCK_50, output wire GPIO_000, 
+output wire GPIO_001, output wire GPIO_003,
+output wire GPIO_005 ,output wire GPIO_007);
+
+vga vögeah(.CLOCK_50(CLOCK_50), .o_hsync(GPIO_), .o_vsync(GPIO_), 
+.o_red(GPIO_), .o_grn(GPIO_), .o_blu(GPIO_));
 
 endmodule
