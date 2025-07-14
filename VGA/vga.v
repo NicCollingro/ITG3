9db3ee1b8bd242810577d4db76e6e39b925c9c39
leidus <aljoscha.becker1@gmail.com>
2025-07-14 18:20:47 +0200

    huso

--- VGA/vga.v
+++ VGA/vga.v
@@ -17,9 +17,17 @@ wire o_hblank, o_vblank;
 hsync hippi ( .i_clk(clk), .o_hsync(o_hsync), .o_hblank(o_hblank));
 vsync vsynchi ( .i_clk(o_blank), .o_vsync(o_vsync), .o_vblank(o_vblank));
 
+reg [11:0] test= 12'd100;
+
 always @(posedge clk) begin
-    ix[y] <= o_hblank ? 0 : ix + 1;
+    ix[y] <= ix + 1;
+    if ((ix - 5)*(ix - 5)+ (y-5)*(y-5) == test)
+end
+
+always @(negedge o_hblank) begin
     y <= o_vblank ? 0 : y + 1;
 end
+
+
 
 endmodule
