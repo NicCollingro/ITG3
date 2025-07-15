a627d4f09fb30010689ced5a86e92f98c1ad9f09
NicCollingro <nic@collingro.de>
2025-07-15 11:49:38 +0200

    .

--- VGA_Nic/VGA.v
+++ VGA_Nic/VGA.v
@@ -51,8 +51,8 @@ module vga(input wire CLOCK_50, input wire [1:0] i_sel, output wire o_hsync, out
         cnt3 =  cnt3 + 1;
     
     reg [2:0] color = 3'b0;
-    always @((x*x + y*y == radius*radius)) begin
-        color = {cnt1[6], cnt1[7], cnt1[8]} | 3*{(x == 640 || x == 1 || y == 480 || y == 1) ? 1'b1 : 1'b0};
+    always @(*) begin
+        (x*x + y*y == radius*radius) ? color = {cnt1[6], cnt1[7], cnt1[8]} | 3*{(x == 640 || x == 1 || y == 480 || y == 1) ? 1'b1 : 1'b0} : color = {0,0,0};
     end
 
 assign {o_red, o_grn, o_blu} = color;
