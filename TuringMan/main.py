2b971730a4c6b07d477242acdab9ad766eb15431
ali fatali <aljoscha.becker1@gmail.com>
2025-07-18 13:30:09 +0200

    EIER

--- TuringMan/main.py
+++ TuringMan/main.py
@@ -1,5 +1,12 @@
-delta = { # move right to start of number block
-( ' q0 ' , '_ ') : ( ' q0 ' , '_ ' , 'R ') ,#TODO
+import TuMa
+import itertools as itl
+
+delta = {( ' q0 ' , '_ ') : ( ' q0 ' , '_ ' , 'R ') ,#TODO
 }
-tm = TuMa . TuringMachine ( ' q0 ' , ' qh ' , delta )
-tm . run (1000 , " 1101 " )
+tm = TuMa.TuringMachine( ' q0 ' , ' qh ' , delta )
+tm.run (1000 , " 1101 " )
+
+Sigma = { 'a ' , 'b '}
+for t in itl.product ( Sigma , repeat =3):
+    tm.run (1000, ' '. join (t))
+    print ("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =")
