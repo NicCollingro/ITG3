4750e71a35d2dd0cdd724685572ad73c59d60352
ali fatali <aljoscha.becker1@gmail.com>
2025-07-18 13:51:57 +0200

    HIHIHI2

--- TuringMan/main.py
+++ TuringMan/main.py
@@ -1,8 +1,13 @@
 import TuMa
 import itertools as itl
 
-delta = {( ' q0 ' , '_ ') : ( ' q0 ' , '_ ' , 'R ') ,#TODO
-}
+delta = { ('q0', '_') : ('q0', '_', 'R'),	
+          ('q0', '1') : ('q1', '_', 'R'),
+          ('q1', '1') : ('q1', '1', 'R'),	
+          ('q1', '_') : ('q2', '1', 'L'),	
+          ('q2', '1') : ('q2', '1', 'L'),	
+          ('q2', '_') : ('qh', '_', 'R'),
+        }
 tm = TuMa.TuringMachine( ' q0 ' , ' qh ' , delta )
 tm.run (1000 , " 1101 " )
 
